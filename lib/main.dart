cf4f4112c7d73e0725566d7039884e32a35e08f9
Aishwarya-pulluri <95070431+Aishwarya-pulluri@users.noreply.github.com>
2023-11-23 03:34:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -21,15 +19,15 @@ class _HomeState extends State<Home> {
 
   final List<Message> _msg = <Message>[];
   final TextEditingController _txtController = TextEditingController();
-  void getChatbotReply(String userReply) async {
+
+  void getChatBotReply(String userReply) async {
     _txtController.clear();
     var response = await http.get(Uri.parse(
-        "http://api.brainshop.ai/get?bid=167355&key=CbtBJcCmY96vzXjL&uid=[Aishwarya pulluri]&msg=[$userReply]"));
+        "http://api.brainshop.ai/get?bid=167356&key=e6tFkq6ZISxTtYXn&uid=[dp_25]&msg=[$userReply"));
     var data = jsonDecode(response.body);
     var botReply = data["cnt"];
     Message msg= Message(
       text: botReply,
-      name: "Bot",
       type: false,
     );
     setState(() {
@@ -38,20 +36,15 @@ class _HomeState extends State<Home> {
 
   }
   void handleSubmitted(text) async{
-    if (kDebugMode) {
-      print(text);
-    }
     _txtController.clear();
     Message msg= Message(
       text: text,
-      name: "YOU",
       type: true,
-      //date: DateTime.now(),
     );
     setState(() {
       _msg.insert(0, msg);
     });
-    getChatbotReply(text);
+    getChatBotReply(text);
   }
 
   @override
@@ -60,7 +53,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text('Chat Bot'),
       ),
-      backgroundColor: Colors.cyan[100],
+      backgroundColor: Colors.grey[300],
       body: Column(
         children: [
           Flexible(
@@ -78,7 +71,6 @@ class _HomeState extends State<Home> {
               data: IconThemeData(color: Theme.of(context).cardColor),
               child: Container(
                 margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                //color: Colors.grey[300],
                 child: Row(
                   children: [
                     Flexible(
@@ -109,22 +101,17 @@ class _HomeState extends State<Home> {
 }
 class Message extends StatelessWidget {
   final String text;
-  final String name;
   final bool type;
-  // final DateTime date;
-  // ignore: use_key_in_widget_constructors
   const Message({
     required this.text,
-    required this.name,
     required this.type,
-    //  required this.date,
   });
 
   List<Widget> bot(context) {
     return <Widget>[
       Container(
         margin: const EdgeInsets.only(right: 16.0),
-        child: const CircleAvatar(child: Text('B')),
+        child: const CircleAvatar(child: Text('Bot')),
       ),
       Expanded(
           child: Column(
@@ -157,8 +144,9 @@ class Message extends StatelessWidget {
       Container(
           margin: const EdgeInsets.only(left: 16.0),
           child: const CircleAvatar(
-              backgroundColor: Colors.grey,
-              child: Text('U', style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white)))
+            backgroundColor: Colors.grey,
+            child: Text('You', style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black)),
+          )
       ),
     ];
   }
@@ -170,92 +158,6 @@ class Message extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: type ? me(context) : bot(context),
         )
-    );
-  }
-}
-
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
